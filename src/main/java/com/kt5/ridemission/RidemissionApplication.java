fcdaa418f386b7715b5c3ae231e4490f5e33fc39
kimtaeo <kimtae5@naver.com>
2022-06-07 17:47:28 +0900

    1

---
+++ src/main/java/com/kt5/ridemission/RidemissionApplication.java
@@ -0,0 +1,13 @@
+package com.kt5.ridemission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidemissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidemissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kt5/ridemission/RidemissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt5.ridemission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidemissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
