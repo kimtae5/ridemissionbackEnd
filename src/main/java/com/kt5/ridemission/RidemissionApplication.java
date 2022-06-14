6c5165028e5887334636e7023aec817963e90486
KTO <KTO@KTOnotebook>
2022-06-14 16:34:13 +0900

    20220614

--- src/main/java/com/kt5/ridemission/RidemissionApplication.java
+++ src/main/java/com/kt5/ridemission/RidemissionApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class RidemissionApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(RidemissionApplication.class, args);
 	}
 
